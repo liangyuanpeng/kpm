07cc6ce1a2b9022bc2370d3d7eecb94eddb9eecf
Lan Liang <gcslyp@gmail.com>
2025-05-09 11:24:56 +0800

    u

--- pkg/client/login_test.go
+++ pkg/client/login_test.go
@@ -4,10 +4,27 @@ import "testing"
 
 // go test -timeout 30s -run ^TestLoginOci$ kcl-lang.io/kpm/pkg/client -v
 func TestLoginOci(t *testing.T) {
+	// c := KpmClient{
+	// 	insecureSkipTLSverify: true,
+	// }
+	// //192.168.3.187:5001
+	// hostname := "ghcr.io"
+	// name := "123"
+	// password := ""
+	// err := c.LoginOci(hostname, name, password)
+	// if err != nil {
+	// 	t.Error(err)
+	// }
+}
+
+// go test -timeout 30s -run ^TestLogoutOci$ kcl-lang.io/kpm/pkg/client -v
+func TestLogoutOci(t *testing.T) {
 	c := KpmClient{
-		insecureSkipTLSverify: true,
+		// insecureSkipTLSverify: true,
 	}
-	err := c.LoginOci("192.168.3.187:5001", "test", "1234")
+	//192.168.3.187:5001
+	hostname := "invalid_registry"
+	err := c.LogoutOci(hostname)
 	if err != nil {
 		t.Error(err)
 	}
